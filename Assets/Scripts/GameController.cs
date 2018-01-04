954a90723b1faa6339112f9da0b13566c4551109
Chris Hendrickson <chendri1@gmail.com>
2018-01-04 18:08:24 +0100

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Text
+
+public class GameController : MonoBehaviour {
+
+    // How much cash the player has
+    private float _cash;
+    public float Cash
+    {
+        get
+        {
+            return _cash;
+        }
+        set
+        {
+            _cash = value;
+            cashText.text = "You have: $" + _cash.ToString("0.00");
+        }
+    }
+
+    /// How much cash is automatically earned per second
+    private float _cashPerSecond;
+    public float CashPerSecond
+    {
+        get
+        {
+            return _cashPerSecond;
+        }
+        set
+        {
+            _cashPerSecond = value;
+            rateText.text = "per second: " + _cashPerSecond.ToString("0.0");
+        }
+    }
+
+    [Tooltip("How much cash players will make when they hit the button.")]
+    public float cashPerClick = 1;
+    [Header("Object References")]
+    public Text cashText;
+    public Text rateText;
+
+
+    void Start () {
+        Cash = 0;
+        CashPerSecond = 0;
+    }
+	
+	void Update () {
+        Cash += CashPerSecond * Time.deltaTime;
+    }
+
+    public void ClickedButton()
+    {
+        Cash += cashPerClick;
+    }
+}
---
+++ Assets/Scripts/StoreItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI; //Text
+
+public enum ItemType
+{
+    ClickPower, PerSecondIncrease
+};
+
+public class StoreItem : MonoBehaviour {
+
+    [Tooltip("How much will this upgrade cost.")]
+    public int cost;
+    public ItemType itemType;
+    [Tooltip("If purchased, how much will it increase this")]
+    public float increaseAmount;
+    private int qty;
+    public Text costText;
+    public Text qtyText;
+    private GameController controller;
+    private Button button;
+
+    // Use this for initialization
+    void Start () {
+        qty = 0;
+        qtyText.text = qty.ToString();
+        costText.text = "$" + cost.ToString();
+        button = transform.GetComponent<Button>();
+        // Execute the ButtonClicked function when we click the button
+        button.onClick.AddListener(this.ButtonClicked);
+        // Get a reference to our GameController via code
+        controller = GameObject.FindObjectOfType<GameController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        button.interactable = (controller.Cash >= cost);
+    }
+
+    public void ButtonClicked()
+    {
+        controller.Cash -= cost;
+        switch (itemType)
+        {
+            case ItemType.ClickPower:
+                controller.cashPerClick += increaseAmount;
+                break;
+            case ItemType.PerSecondIncrease:
+                controller.CashPerSecond += increaseAmount;
+                break;
+        }
+
+        qty++;
+        qtyText.text = qty.ToString();
+    }
+}
